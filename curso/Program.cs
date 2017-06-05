02c960e6dbf88cdfda3ebff29a02b5b12149c71d
Nelio Alves <acenelio@gmail.com>
2017-06-05 14:52:28 -0300

    Primeiro commit

---
+++ curso/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using curso.dominio;
+
+namespace curso {
+    class Program {
+        static void Main(string[] args) {
+
+            List<Artista> artistas = new List<Artista>();
+            List<Filme> filmes = new List<Filme>();
+
+            int opcao = 0;
+
+            artistas.Add(new Artista(101, "Scarlett Johansson", 4000000.00));
+            artistas.Add(new Artista(102, "Chris Evans", 2500000.00));
+            artistas.Add(new Artista(103, "Robert Downey Jr.", 3000000.00));
+            artistas.Add(new Artista(104, "Morgan Freeman", 4000000.00));
+            artistas.Sort();
+
+            while (opcao != 5) {
+                Console.Clear();
+                Tela.mostrarMenu();
+                try {
+                    opcao = int.Parse(Console.ReadLine());
+                }
+                catch (Exception e) {
+                    Console.WriteLine("Erro inesperado: " + e.Message);
+                    opcao = 0;
+                }
+                Console.WriteLine();
+
+                if (opcao == 1) {
+                    Tela.mostrarArtistas(artistas);
+                }
+                else if (opcao == 2) {
+                    try {
+                        Artista A = Tela.lerArtista();
+                        artistas.Add(A);
+                        artistas.Sort();
+                    }
+                    catch (Exception e) {
+                        Console.WriteLine("Erro inesperado: " + e.Message);
+                    }
+                }
+                else if (opcao == 3) {
+                    try {
+                        Filme F = Tela.lerFilme(artistas);
+                        filmes.Add(F);
+                    }
+                    catch (ModelException e) {
+                        Console.WriteLine("Erro de negócio: " + e.Message);
+                    }
+                    catch (Exception e) {
+                        Console.WriteLine("Erro inesperado: " + e.Message);
+                    }
+                }
+                else if (opcao == 4) {
+                    try {
+                        Tela.mostrarFilme(filmes);
+                    }
+                    catch (ModelException e) {
+                        Console.WriteLine("Erro de negócio: " + e.Message);
+                    }
+                    catch (Exception e) {
+                        Console.WriteLine("Erro inesperado: " + e.Message);
+                    }
+                }
+                else if (opcao == 5) {
+                    Console.WriteLine("Fim do programa!");
+                }
+                else {
+                    Console.WriteLine("Opção inválida!");
+                    Console.WriteLine();
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ curso/Tela.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using curso.dominio;
+
+namespace curso {
+
+    // Classe responsavel por conter operações que interagem com o usuário
+    // no modo console
+
+    class Tela {
+        public static void mostrarMenu() {
+            Console.WriteLine("1 - Listar artistas ordenadamente");
+            Console.WriteLine("2 - Cadastrar artista");
+            Console.WriteLine("3 - Cadastrar filme");
+            Console.WriteLine("4 - Mostrar dados de um filme");
+            Console.WriteLine("5 - Sair");
+            Console.Write("Digite uma opção: ");
+        }
+
+        public static void mostrarArtistas(List<Artista> artistas) {
+            Console.WriteLine("LISTAGEM DE ARTISTAS:");
+            for (int i = 0; i < artistas.Count; i++) {
+                Console.WriteLine(artistas[i]);
+            }
+            Console.WriteLine();
+        }
+
+        public static Artista lerArtista() {
+            Console.WriteLine("Digite os dados do artista: ");
+            Console.Write("Código: ");
+            int codigo = int.Parse(Console.ReadLine());
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Valor do cachê: ");
+            double cache = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            return new Artista(codigo, nome, cache);
+        }
+
+        public static Filme lerFilme(List<Artista> artistas) {
+            Console.WriteLine("Digite os dados do filme: ");
+            Console.Write("Código: ");
+            int codigo = int.Parse(Console.ReadLine());
+            Console.Write("Título: ");
+            string titulo = Console.ReadLine();
+            Console.Write("Ano: ");
+            int ano = int.Parse(Console.ReadLine());
+            Filme F = new Filme(codigo, titulo, ano);
+            Console.Write("Quantas participações tem o filme? ");
+            int N = int.Parse(Console.ReadLine());
+            for (int i=1; i<=N; i++) {
+                Console.WriteLine("Digite os dados da " + i + "ª participação:");
+                Console.Write("Artista (código): ");
+                int codArtista = int.Parse(Console.ReadLine());
+                int pos = artistas.FindIndex(x => x.codigo == codArtista);
+                if (pos == -1) {
+                    throw new ModelException("Código de artista não encontrado: " + codArtista);
+                }
+                Console.Write("Desconto: ");
+                double desconto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Participacao part = new Participacao(desconto, artistas[pos], F);
+                F.participacoes.Add(part);
+            }
+            return F;
+        }
+
+        public static void mostrarFilme(List<Filme> filmes) {
+            Console.Write("Digite o código do filme: ");
+            int codFilme = int.Parse(Console.ReadLine());
+            int pos = filmes.FindIndex(x => x.codigo == codFilme);
+            if (pos == -1) {
+                throw new ModelException("Código de filme não encontrado: " + codFilme);
+            }
+            Console.WriteLine(filmes[pos]);
+            Console.WriteLine();
+        }
+    }
+}
